b12938898df8f537c1f6aa2090666ac1ee0d0182
Huse Kivrak <huse7@pm.me>
2023-04-06 15:32:25 -0400

    added company routes for GET, POST, PUT methods

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 const express = require("express");
 const { NotFoundError } = require("./expressError");
+// const db = require("./db");
+const companies = require("./routes/companies");
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/companies', companies);
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
---
+++ routes/companies.js
@@ -0,0 +1,73 @@
+const router = express.Router();
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../expressError");
+
+/** Returns list of all companies {companies: [{code, name}, ...]} */
+router.get("/", async function (req, res) {
+  const result = await db.query(
+    `SELECT code, name
+      FROM companies`
+  );
+  const companies = result.rows;
+  return res.status(200).json({ companies });
+});
+
+/** Returns company object at code: {company: {code, name, description}} */
+
+router.get("/:code", async function (req, res) {
+  const result = await db.query(
+    `SELECT code, name, description
+      FROM companies
+      WHERE code = $1`,
+    [req.params.code]
+  );
+  const company = result.rows[0];
+
+  if (!company) throw new NotFoundError("Company code not found.");
+  return res.status(200).json({ company });
+});
+
+/**
+ * Adds a company to biztime.
+ * Params: {code, name, description}
+ * Returns {company: {code, name, description}}  */
+
+router.post("/", async function (req, res) {
+  const { code, name, description } = req.body;
+  if (!code || !name || !description)
+    throw new BadRequestError("Invalid request data.");
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+      VALUES ($1, $2, $3)
+      RETURNING code, name, description`,
+    [code, name, description]
+  );
+
+  const company = result.rows[0];
+  return res.status(201).json({ company });
+});
+
+/**
+ * Edits an exisiting company
+ * Params: {name, description}
+ * Returns: {company: {code, name, description}}*/
+
+router.put("/:code", async function (req, res) {
+  const { name, description } = req.body;
+  if (!name || !description) throw new BadRequestError("Invalid request data.");
+
+  const result = await db.query(
+    `UPDATE companies
+      SET name = $2,
+          description = $3
+      WHERE code = $1
+      RETURNING code, name, description`,
+      [req.params.code, name, description]
+  )
+  const company = result.rows[0];
+  return res.json({company});
+});
+
+
+
+module.exports = companies;
